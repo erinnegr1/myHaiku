f7f30868884f163c13f48502ce28f879016db2b2
Erinnegr1 <erinne.grant@gmail.com>
2022-11-20 12:11:43 -0500

    setting create-react-app up. Installed OpenAI

---
+++ my-haiku/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Get ready for my hAIku
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
